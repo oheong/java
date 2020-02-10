74a556c2d7fe6ae543483cc88cfabbbed0e3c5ab
OHEONG <enffl9568@naver.com>
2020-02-10 12:01:31 +0900

    day5 workspace  class 상속

---
+++ day05_WS_TV.java
@@ -0,0 +1,24 @@
+package com.ssafy.java.day05.ws;
+
+public class TV extends Product{
+	private int inch;
+
+	TV(){
+		
+	}
+	public TV(String num, String name, int price, int jaego, int inch) {
+		super(num, name, price, jaego);
+		this.inch = inch;
+	}
+
+	public int getInch() {
+		return inch;
+	}
+
+	public void setInch(int inch) {
+		this.inch = inch;
+	}
+	public String toString() {
+		return  "모델 넘버 : "+super.getNum() + "\t 모델 이름 : " + super.getName() + "\t 가격 : " + super.getPrice() + "\t 재고 : " + super.getJaego() + "\t 인치 : " + getInch() + ".";
+	}
+}
